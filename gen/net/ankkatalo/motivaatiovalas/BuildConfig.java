63f392c818791c26b1270de1121ef84c300355e2
U-lappyduck\jussi <jussi@lappyduck.(none)>
2012-04-18 19:00:28 +0300

    initial android skeleton add

---
+++ gen/net/ankkatalo/motivaatiovalas/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.ankkatalo.motivaatiovalas;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/net/ankkatalo/motivaatiovalas/MotivaatiovalasActivity.java
@@ -0,0 +1,13 @@
+package net.ankkatalo.motivaatiovalas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MotivaatiovalasActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
