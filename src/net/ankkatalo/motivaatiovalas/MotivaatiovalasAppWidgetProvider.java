4731ae3a86eb0c79f36bd486923889321c6611ae
Jussi Niskanen <jussini@pcft-jussini-li.sasken.com>
2012-05-08 14:57:45 +0300

    add onReceive methed for debug purposes

--- src/net/ankkatalo/motivaatiovalas/MotivaatiovalasAppWidgetProvider.java
+++ src/net/ankkatalo/motivaatiovalas/MotivaatiovalasAppWidgetProvider.java
@@ -9,8 +9,10 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class MotivaatiovalasAppWidgetProvider extends AppWidgetProvider {
 
@@ -43,6 +45,26 @@ public class MotivaatiovalasAppWidgetProvider extends AppWidgetProvider {
 		}
 	}
 	
+    public void onReceive(Context context, Intent intent) {
+    	Log.d(TAG, String.format("onReceive, intent action: %s", intent.getAction()));
+    	
+    	if (intent.getAction().equals("android.appwidget.action.APPWIDGET_UPDATE")) {
+    		int appWidgetId = intent.getIntExtra(
+    				AppWidgetManager.EXTRA_APPWIDGET_ID,
+    		        AppWidgetManager.INVALID_APPWIDGET_ID);
+        	Log.d(TAG, String.format("onReceive, appWidgetId: %d", appWidgetId));    		
+    	}
+    	
+    	//AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        //if (intent.getAction().equals(TOAST_ACTION)) {
+        //    int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+        //            AppWidgetManager.INVALID_APPWIDGET_ID);
+        //    int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+        //    Toast.makeText(context, "Touched view " + viewIndex, Toast.LENGTH_SHORT).show();
+        //}
+        super.onReceive(context, intent);
+    }
+	
 	public void onUpdate(Context context, AppWidgetManager manager, int[] appWidgetIds) {
 		
 		
@@ -62,22 +84,30 @@ public class MotivaatiovalasAppWidgetProvider extends AppWidgetProvider {
 		for (int i = 0; i < m_activeAppWidgetIds.size(); ++i) {
 			widgetIds[i] = ((Integer)m_activeAppWidgetIds.toArray()[i]).intValue(); // pretty...			
 		}
+	
 
 		// register an onClick listener to update view
 		Intent intent = new Intent(context, MotivaatiovalasAppWidgetProvider.class);
 	
 		intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
-
+		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+		
+		            
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
 				0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 		views.setOnClickPendingIntent(R.id.imageView1, pendingIntent);
-				
+		
+		
 		// set a new random whale image to every widget
-		for(int appWidgetId : m_activeAppWidgetIds) {									
-			int i = r.nextInt(numMessages);		
+//		for(int appWidgetId : m_activeAppWidgetIds) {
+		for(int appWidgetId : appWidgetIds) {
+			int i = r.nextInt(numMessages);
+					
 			views.setImageViewResource(R.id.imageView1, messages[i]);						
 			manager.updateAppWidget(appWidgetId, views);			
 		}
+		
+		super.onUpdate(context, manager, appWidgetIds);
+		
 	}	
 }
